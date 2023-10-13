2eb6c98214613dc489d9ab1105317725c3e5797b
TaroM3 <taro.melillo@gmail.com>
2023-10-13 10:00:12 -0300

    useFirebase.jsx modificado

--- src/hooks/useFirebase.jsx
+++ src/hooks/useFirebase.jsx
@@ -1,20 +1,28 @@
-import { collection, getDocs } from "firebase/firestore"
+import { collection, doc, getDoc, getDocs } from "firebase/firestore"
 import { useEffect, useState } from "react"
 import { db } from "../firebaseConfig"
-
 //entra por parametro el nombre de la base de datos y devuelve un array de objetos
-const useFirestore = (databaseName) => {
-    const [data, setData] = useState([])
+const useFirestore = (databaseName, id = undefined) => {
+    
+    const [data, setData] = useState({})
 
     useEffect(() => {
-        const getData = collection(db, databaseName)
-        getDocs(getData).then( (res) => {
-            let arrayData = res.docs.map((data) => {
-                return {...data.data(), id: data.id}
+        const getData = collection(db, databaseName);
+
+        if (id !== undefined) {
+            const getRef = doc(getData, id)
+            getDoc(getRef).then( (res) => {
+                setData(res.data(), res.id)
+                })
+        }else{
+            getDocs(getData).then( (res) => {
+                let arrayData = res.docs.map((data) => {
+                    return {...data.data(), id: data.id}
+                })
+                setData(arrayData)
             })
-            setData(arrayData)
-        })
-    }, [databaseName])
+        }
+    }, [databaseName, id])
   return [data]
 }
 
