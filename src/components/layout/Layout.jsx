86aa5d2e365aef07b18d79c55d00e259244d159d
TaroM3 <taro.melillo@gmail.com>
2023-10-27 09:12:02 -0300

    Layout.jsx Modificado

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,14 +1,22 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./footer/Footer";
 import NavbarContainer from "./navbar/NavbarContainer";
+import { Box } from "@mui/material";
 
 const Layout = () => {
   return (
-    <>
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
       <NavbarContainer />
       <Outlet />
       <Footer />
-    </>
+    </Box>
   );
 };
 
