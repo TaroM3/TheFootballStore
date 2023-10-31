e0224ace2ccc28d478ed9f5a32ee584c82f1cd24
Luciana Alonso <lucianaalonso98@gmail.com>
2023-10-30 22:45:38 -0300

    Info y envios

--- src/components/page/contacto/Contacto.jsx
+++ src/components/page/contacto/Contacto.jsx
@@ -1,5 +1,43 @@
+import { Breadcrumbs, Link, SvgIcon, Typography } from "@mui/material";
+import styles from "./Contacto.module.css";
+import InstagramIcon from "@mui/icons-material/Instagram";
 const Contacto = () => {
-  return <div>Contacto</div>;
+  return (
+    <div className={styles.contacto}>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link
+          style={{ fontFamily: "frontpageneue" }}
+          underline="hover"
+          color="var(--main)"
+          href="/"
+        >
+          Inicio
+        </Link>
+        <Typography
+          style={{ fontFamily: "frontpageneue", textTransform: "capitalize" }}
+          color="var(--main)"
+        >
+          Contacto
+        </Typography>
+      </Breadcrumbs>
+      <section className={styles.contactoDivs}>
+        <div>
+          <h4>Contacto</h4>
+          <p>
+            Contactanos blablabal ? Lorem ipsum dolor sit amet consectetur.
+            Massa purus commodo faucibus id diam amet ultricies risus.{" "}
+          </p>
+          <p>+54 9 221 669-7039</p>
+          <Link to={"/info"}>
+            <SvgIcon component={InstagramIcon} />
+          </Link>
+        </div>
+        <div>
+          <h5>¡Mandanos un mensaje!</h5>
+        </div>{" "}
+      </section>
+    </div>
+  );
 };
 
 export default Contacto;
