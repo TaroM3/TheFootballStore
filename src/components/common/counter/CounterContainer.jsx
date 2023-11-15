bfbdb584e6b98cf3973e2d7ea96f6bb637c6144d
TaroM3 <taro.melillo@gmail.com>
2023-11-15 09:26:32 -0300

    CartProductCard agregado

--- src/components/common/counter/CounterContainer.jsx
+++ src/components/common/counter/CounterContainer.jsx
@@ -1,22 +1,8 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import Counter from "./Counter";
-import { CartContext } from "../../../context/CartContext";
-const CounterContainer = ({
-  quantity = 1,
-  quantityGetter = null,
-  id = null,
-}) => {
-  const { updateQuantityById } = useContext(CartContext);
 
+const CounterContainer = ({ quantity = 1, quantityGetter = null }) => {
   const [counter, setCounter] = useState(quantity);
-
-  if (quantityGetter !== null) {
-    quantityGetter(counter);
-  }
-  // if (id !== null) {
-  //   updateQuantityById(id, counter);
-  //   // console.log("HOla");
-  // }
 
   return <Counter counter={counter} setCounter={setCounter} />;
 };
