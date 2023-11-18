d1a40d4edaaf2ab3bdd8d7b5ab8fb831f51100d1
Luciana Alonso <lucianaalonso98@gmail.com>
2023-11-18 12:44:12 -0300

    env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,24 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAFlqg9J7Y6zytHNY-isOiPvgZlK1i5EQ8",
-//   authDomain: "thefootballstore-141f0.firebaseapp.com",
-//   projectId: "thefootballstore-141f0",
-//   storageBucket: "thefootballstore-141f0.appspot.com",
-//   messagingSenderId: "636226002574",
-//   appId: "1:636226002574:web:cca19ff003a2904505f3df",
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCf3Gy9Cw06EBGhYmqLVmDjbk9pnqE37W4",
-  authDomain: "ecommerce-566e0.firebaseapp.com",
-  projectId: "ecommerce-566e0",
-  storageBucket: "ecommerce-566e0.appspot.com",
-  messagingSenderId: "616815970518",
-  appId: "1:616815970518:web:9a3d0434c3fad40e2c2177",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
-
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
